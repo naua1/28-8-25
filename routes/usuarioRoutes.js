5d95d5226c746a9a2057f69df73f26b98b15d40a
Nauã <90857896+naua1@users.noreply.github.com>
2025-09-15 22:29:38 +0000

    api

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const controller = require('../controllers/usuarioController');
+
+
+router.post('/registrar', controller.registrar);
+
+router.post('/login', controller.login);
+
+
+module.exports = router
